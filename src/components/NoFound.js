16c036ad879d6d496fcfb110228d353ca85508b1
David McGarvey <davidjmcgarvey+github@gmail.com>
2019-11-19 09:41:42 -0800

    first commit

---
+++ src/components/NoFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div class="photo-container">
+        <h2>Results</h2>
+        <ul>
+          <li class="not-found">
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+          </li>
+        </ul>
+      </div>
+  )
+}
+
+export default NotFound
