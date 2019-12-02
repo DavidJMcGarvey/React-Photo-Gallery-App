2e7e7baea882541b94985cb61f10f71c348be597
David McGarvey <davey@Davids-MacBook-Air.local>
2019-12-02 15:51:06 -0800

    Updates

---
+++ precache-manifest.5c69645634bfcd566d8cabbc12037e39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20747ad9ae71150fe888df5caf03aec1",
+    "url": "/FSJS-techdegree-project-07/index.html"
+  },
+  {
+    "revision": "b11071e1e0ba7222e81e",
+    "url": "/FSJS-techdegree-project-07/static/css/main.f09a83c4.chunk.css"
+  },
+  {
+    "revision": "a1da08a141039eafb19f",
+    "url": "/FSJS-techdegree-project-07/static/js/2.f128c55d.chunk.js"
+  },
+  {
+    "revision": "b11071e1e0ba7222e81e",
+    "url": "/FSJS-techdegree-project-07/static/js/main.d0042357.chunk.js"
+  },
+  {
+    "revision": "2f6ececf029c6a0ee0cb",
+    "url": "/FSJS-techdegree-project-07/static/js/runtime-main.f82da223.js"
+  }
+]);
